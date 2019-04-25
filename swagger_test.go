76fb3e43fc8892ed018602f1eb8c88df29dfe005
samous <samuel.kerboeuf@gmail.com>
2019-04-25 12:22:58 +0200

    Update swagger_test.go

--- swagger_test.go
+++ swagger_test.go
@@ -8,9 +8,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
-	"github.com/swaggo/gin-swagger/swaggerFiles"
+	"github.com/samous/gin-swagger/swaggerFiles"
 
-	_ "github.com/swaggo/gin-swagger/example/basic/docs"
+	_ "github.com/samous/gin-swagger/example/basic/docs"
 )
 
 func TestWrapHandler(t *testing.T) {
